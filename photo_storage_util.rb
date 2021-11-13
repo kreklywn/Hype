787363f9838713573c7c967a1be34cff9616db62
landoshepherd <landolshepherd@gmail.com>
2021-11-12 22:08:02 -0500

    Updated documentation

--- photo_storage_util.rb
+++ photo_storage_util.rb
@@ -1,5 +1,6 @@
+# Returns a list of valid storage options for photos
 def get_list_of_storage_options()
-    #Get list of external hard drives currently connected to the jetson
+    # Get list of external hard drives currently connected to the jetson
     usb_drives = %x`ls /media/$USER`
     list_usb_drives = usb_drives.split("\n")
 
@@ -7,31 +8,28 @@ def get_list_of_storage_options()
     return list_usb_drives
 end
 
-#Returns the path to which the photos will be saved depending on
-#the user's selection.
+# Returns the path to which the photos will be saved depending on
+# the user's device selection.
 def get_photo_directory_path(selection="Default")
     user = %x`echo $USER`.delete("\n")
     photos_directory = '/HyperRailPhotos'
 
     if selection == "Default"
-        #Default location for photos if external drive not connected
+        # Default location for photos if external drive not connected
         dir_path = '/home/' + user + photos_directory
 
         if (!Dir.exist?(dir_path))
             %x`mkdir ~#{photos_directory}`
             puts("Created directory")
         end
-        #Assign path variable to PHOTO_STORAGE_DIR const variable here
+        # Assign path variable to PHOTO_STORAGE_DIR const variable here
 
         return dir_path
     else
-        #Maybe we should create an environment variable or global constant called PHOTO_STORAGE_DIR
-        #and assign it this path. That way, the ROS node dedicated to capturing images will know
-        #where to save them.
+        # Maybe we should create an environment variable or global constant called PHOTO_STORAGE_DIR
+        # and assign it this path. That way, the ROS node dedicated to capturing images will know
+        # where to save them.
         path = '/media/' + user.delete("\n") + "/" + selection + photos_directory
         return path
     end
 end
-
-puts("Photos Directory: " + get_photo_directory_path("Default"))
-puts("List of Devices: ", get_list_of_storage_options())
